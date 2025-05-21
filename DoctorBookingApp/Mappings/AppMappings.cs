75c4ed23de66fa4fa9905f3392d60ca0717113af
Basim Hilal <basimhilalofficial@gmail.com>
2025-05-21 11:00:32 +0530

    App mappings configured

---
+++ DoctorBookingApp/Mappings/AppMappings.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DoctorBookingApp.Models.UserModel;
+using DoctorBookingApp.Models.UserModel.Dto;
+
+namespace DoctorBookingApp.Mappings
+{
+    public class AppMappings : Profile
+    {
+        public AppMappings()
+        {
+            CreateMap<User, UserRegDto>().ReverseMap();
+            CreateMap<User, UserRegResDto>().ReverseMap();
+        }
+    }
+}
