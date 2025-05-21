eb55f8b960acfcd587f22e4dcccd7bd869021753
Basim Hilal <basimhilalofficial@gmail.com>
2025-05-21 11:52:23 +0530

    AuthService methods are defined

---
+++ DoctorBookingApp/Services/AuthService/AuthService.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using BCrypt.Net;
+using DoctorBookingApp.Data;
+using DoctorBookingApp.Models.UserModel;
+using DoctorBookingApp.Models.UserModel.Dto;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DoctorBookingApp.Services.AuthService
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
+        private readonly AppDbContext _context;
+        public AuthService(IConfiguration config, IMapper mapper, AppDbContext context)
+        {
+            _config = config;
+            _mapper = mapper;
+            _context = context;
+        }
+        public async Task<UserLogResDto> Login(UserLoginDto request)
+        {
+            try
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+                if (user is null) throw new InvalidOperationException("Invalid Email");
+                if (!BCrypt.Net.BCrypt.Verify(request.Password, user.Password)) throw new InvalidOperationException("Invalid Password");
+
+                var token = CreateToken(user);
+
+                var loginRes = new UserLogResDto
+                {
+                    Username = user.Username,
+                    Email = user.Email,
+                    Role = user.Role,
+                    token = token
+                };
+                return loginRes;
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<string> Register(UserRegDto request)
+        {
+            try
+            {
+                if (request is null) throw new ArgumentNullException("Request can not be null");
+
+                var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+                if (existingUser != null) throw new Exception("User with same Email already exists");
+
+                string salt = BCrypt.Net.BCrypt.GenerateSalt();
+                string hashpass = BCrypt.Net.BCrypt.HashPassword(request.Password, salt);
+
+                var user = _mapper.Map<User>(request);
+                user.Password = hashpass;
+
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return "User Registered Successfully";
+
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private string CreateToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                signingCredentials: credentials,
+                expires: DateTime.UtcNow.AddHours(2)
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ DoctorBookingApp/Services/AuthService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using DoctorBookingApp.Models.UserModel.Dto;
+
+namespace DoctorBookingApp.Services.AuthService
+{
+    public interface IAuthService
+    {
+        public Task<string> Register(UserRegDto request);
+        public Task<UserLogResDto> Login(UserLoginDto request);
+    }
+}
