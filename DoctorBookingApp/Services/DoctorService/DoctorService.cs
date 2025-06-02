e01b6995838a9ae77db4ac582df47bb0f50c650e
Basim Hilal <basimhilalofficial@gmail.com>
2025-06-02 15:09:46 +0530

    Remove time slot by date

--- DoctorBookingApp/Services/DoctorService/DoctorService.cs
+++ DoctorBookingApp/Services/DoctorService/DoctorService.cs
@@ -98,6 +98,22 @@ namespace DoctorBookingApp.Services.DoctorService
                 throw new Exception(ex.Message);
             }
         }
+        public async Task<string> RemoveTimeSlotByDate(Guid userId,  DateTime date)
+        {
+            try
+            {
+                var doctor = await _context.Doctors.FirstOrDefaultAsync(d => d.UserId == userId);
+                if (doctor is null) throw new Exception("Doctor profile not available");
+                var timeSlots = await _context.TimeSlots.Where(t=> t.DoctorId == doctor.Id && t.SlotDate == date).ToListAsync();
+                if (!timeSlots.Any()) throw new Exception("Time Slots are not available");
+                _context.TimeSlots.RemoveRange(timeSlots);
+                await _context.SaveChangesAsync();
+                return "All time slots removed";
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
         public async Task<string> CreateDoctorProfile(Guid userId, DoctorReqDto request)
         {
             try
